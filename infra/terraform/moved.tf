67c97a86bff69e241bc8da5df3ad58c4cd8f10d7
Badis Merakchi <badis.merakchi@gmail.com>
2026-07-01 14:35:20 +0200

    Remove Terraform moved blocks after state migration

--- infra/terraform/moved.tf
+++
@@ -1,9 +0,0 @@
-moved {
-  from = azurerm_resource_group.main
-  to   = module.resource_group.azurerm_resource_group.main
-}
-
-moved {
-  from = azurerm_container_registry.main
-  to   = module.acr.azurerm_container_registry.main
-}
